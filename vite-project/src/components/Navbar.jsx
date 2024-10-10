2eec4b8b6489ca50592c17ad9c0b73ddb3ae275b
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2024-10-11 00:58:17 +0530

    adjustments

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import ElicitLogo from '../assets/elicit-logo.png';
 
-export default function Navbar(){
-  return(
-      <div className='bg-black text-white sticky top-0 h-20 flex p-4 justify-center shadow-[0_4px_6px_-1px_rgba(239,68,68,0.5),0_2px_4px_-2px_rgba(239,68,68,0.5)]'>
-        <div className='flex justify-between items-center'>
-          <img src={ElicitLogo} alt="Elicit Logo" className='h-12'/>
-          <p className='text-xl font-sans font-semibold'>Elicit 2024</p>
-        </div>
+export default function Navbar() {
+  return (
+    <div className='bg-black text-white fixed top-0 left-0 right-0 h-20 flex p-4 justify-center shadow-[0_4px_6px_-1px_rgba(239,68,68,0.5),0_2px_4px_-2px_rgba(239,68,68,0.5)] z-50'>
+      <div className='flex justify-between items-center w-full max-w-screen-xl mx-auto'>
+        <img src={ElicitLogo} alt="Elicit Logo" className='h-12' />
+        <p className='text-xl font-sans font-semibold'>Elicit 2024</p>
       </div>
+    </div>
   )
 }
