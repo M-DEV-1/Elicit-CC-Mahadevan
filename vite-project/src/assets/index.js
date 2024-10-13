52e06900c163c87c44c5adf1af38a2229673f332
M-DEV-1 <mahadevankizhakkedathu@gmail.com>
2024-10-13 16:01:58 +0530

    added: webp images, general formatting | removed: jpg images

--- vite-project/src/assets/index.js
+++ vite-project/src/assets/index.js
@@ -5,264 +5,275 @@ const images = [
     {
       name: "Anvi Sarbhai",
       committee: "Creatives",
-      image: 'src/assets/images/Creatives/Anvi_Sarbhai.jpg',
+      image: 'src/assets/images/Creatives/Anvi_Sarbhai.webp',
       
     },
     {
       name: "Apurv Harsh",
       committee: "Creatives",
-      image: 'src/assets/images/Creatives/Apurv_Harsh.jpg',
+      image: 'src/assets/images/Creatives/Apurv_Harsh.webp',
     },
     {
       name: "Manalee",
       committee: "Creatives",
-      image: 'src/assets/images/Creatives/Manalee.jpg',
+      image: 'src/assets/images/Creatives/Manalee.webp',
     },
     {
       name: "Sanyam Kathed",
       committee: "Creatives",
-      image: 'src/assets/images/Creatives/Sanyam_Kathed.jpg',
+      image: 'src/assets/images/Creatives/Sanyam_Kathed.webp',
     },
     {
       name: "Vansh Vashist",
       committee: "Creatives",
-      image: 'src/assets/images/Creatives/Vansh_Vashist.jpg',
+      image: 'src/assets/images/Creatives/Vansh_Vashist.webp',
+    },
+    // Web Dev committee
+    {
+      name: "Amrisha Srivastava",
+      committee: "Web Dev",
+      image: 'src/assets/images/WebDev/Amrisha_Srivastava.webp',
+    },
+    {
+      name: "Mahadevan KS",
+      committee: "Web Dev",
+      image: 'src/assets/images/WebDev/Mahadevan_KS.webp',
+    },
+    {
+      name: "Oshika_Sharma",
+      committee: "Web Dev",
+      image: 'src/assets/images/WebDev/Oshika_Sharma.webp',
     },
     // Events committee
     {
       name: "Eshaan Saha",
       committee: "Events",
-      image: 'src/assets/images/Events/Eshaan_Saha.jpg',
+      image: 'src/assets/images/Events/Eshaan_Saha.webp',
     },
     {
       name: "Eshanya Padial",
       committee: "Events",
-      image: 'src/assets/images/Events/Eshanya_Padial.jpg',
+      image: 'src/assets/images/Events/Eshanya_Padial.webp',
     },
     {
       name: "Mahak Agrawal",
       committee: "Events",
-      image: 'src/assets/images/Events/Mahak_Agrawal.jpg',
+      image: 'src/assets/images/Events/Mahak_Agrawal.webp',
     },
     {
       name: "Manan Dubey",
       committee: "Events",
-      image: 'src/assets/images/Events/Manan_Dubey.jpg',
+      image: 'src/assets/images/Events/Manan_Dubey.webp',
     },
     {
       name: "Osh Gupta",
       committee: "Events",
-      image: 'src/assets/images/Events/Osh_Gupta.jpg',
+      image: 'src/assets/images/Events/Osh_Gupta.webp',
     },
     {
       name: "Pranav Deshpande",
       committee: "Events",
-      image: 'src/assets/images/Events/Pranav_Deshpande.jpg',
+      image: 'src/assets/images/Events/Pranav_Deshpande.webp',
     },
     {
       name: "Pranjal Sharma",
       committee: "Events",
-      image: 'src/assets/images/Events/Pranjal_Sharma.jpg',
+      image: 'src/assets/images/Events/Pranjal_Sharma.webp',
     },
     {
       name: "Shivika Singh",
       committee: "Events",
-      image: 'src/assets/images/Events/Shivika_Singh.jpg',
+      image: 'src/assets/images/Events/Shivika_Singh.webp',
     },
     {
       name: "Shreya Singh",
       committee: "Events",
-      image: 'src/assets/images/Events/Shreya_Singh.jpg',
+      image: 'src/assets/images/Events/Shreya_Singh.webp',
     },
     {
       name: "Veer Sirohia",
       committee: "Events",
-      image: 'src/assets/images/Events/Veer_Sirohia.jpg',
+      image: 'src/assets/images/Events/Veer_Sirohia.webp',
     },
     // Finance committee
     {
       name: "Akshat Mishra",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Akshat_Mishra.jpg',
+      image: 'src/assets/images/Finance/Akshat_Mishra.webp',
     },
     {
       name: "Anish Mahapatra",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Anish_Mahapatra.jpg',
+      image: 'src/assets/images/Finance/Anish_Mahapatra.webp',
     },
     {
       name: "Anshika Kumari",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Anshika_Kumari.jpg',
+      image: 'src/assets/images/Finance/Anshika_Kumari.webp',
     },
     {
       name: "Gurleen Kaur",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Gurleen_Kaur.jpg',
+      image: 'src/assets/images/Finance/Gurleen_Kaur.webp',
     },
     {
       name: "Nitya Jain",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Nitya_Jain.jpg',
+      image: 'src/assets/images/Finance/Nitya_Jain.webp',
     },
     {
       name: "Riddhi Batra",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Riddhi_Batra.jpg',
+      image: 'src/assets/images/Finance/Riddhi_Batra.webp',
     },
     {
       name: "Shikha Srivastava",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Shikha_Srivastava.jpg',
+      image: 'src/assets/images/Finance/Shikha_Srivastava.webp',
     },
     {
       name: "Tarni Khatri",
       committee: "Finance",
-      image: 'src/assets/images/Finance/Tarni_Khatri.jpg',
+      image: 'src/assets/images/Finance/Tarni_Khatri.webp',
     },
     // Marketing committee
     {
       name: "Aashi Singh",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Aashi_Singh.jpg',
+      image: 'src/assets/images/Marketing/Aashi_Singh.webp',
     },
     {
       name: "Adveita Bhargava",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Adveita_Bhargava.jpg',
+      image: 'src/assets/images/Marketing/Adveita_Bhargava.webp',
     },
     {
       name: "Aishwika Gupta",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Aishwika_Gupta.jpg',
+      image: 'src/assets/images/Marketing/Aishwika_Gupta.webp',
     },
     {
       name: "Aviral Dimri",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Aviral_Dimri.jpg',
+      image: 'src/assets/images/Marketing/Aviral_Dimri.webp',
     },
     {
       name: "Disha Manchanda",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Disha_Manchanda.jpg',
+      image: 'src/assets/images/Marketing/Disha_Manchanda.webp',
     },
     {
       name: "Harman Bhambra",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Harman_Bhambra.jpg',
+      image: 'src/assets/images/Marketing/Harman_Bhambra.webp',
     },
     {
       name: "Mrinal Malik",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Mrinal_Malik.jpg',
+      image: 'src/assets/images/Marketing/Mrinal_Malik.webp',
     },
     {
       name: "Nivan Vashisht",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Nivan_Vashisht.jpg',
+      image: 'src/assets/images/Marketing/Nivan_Vashist.webp',
     },
     {
       name: "Palak Bhaiya",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Palak_Bhaiya.jpg',
+      image: 'src/assets/images/Marketing/Palak_Bhaiya.webp',
     },
     {
       name: "Pranjana",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Pranjana_Rahangdale.jpg',
+      image: 'src/assets/images/Marketing/Pranjana_Rahangdale.webp',
     },
     {
       name: "Shifa Khan",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Shifa_Khan.jpg',
+      image: 'src/assets/images/Marketing/Shifa_Khan.webp',
     },
     {
       name: "Tamanna Bhargava",
       committee: "Marketing",
-      image: 'src/assets/images/Marketing/Tamanna_Bhargava.jpg',
-    },
-    // Media committee
-    {
-      name: "Arif Islam Shaik",
-      committee: "Media",
-      image: 'src/assets/images/Media/Arif_Islam_Shaik.jpg',
-    },
-    {
-      name: "Rishit Jaiswal",
-      committee: "Media",
-      image: 'src/assets/images/Media/Rishit_Jaiswal.jpg',
+      image: 'src/assets/images/Marketing/Tamanna_Bhargava.webp',
     },
     // Operations committee
     {
       name: "Akash Kumar",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Akash_Kumar.jpg',
+      image: 'src/assets/images/Operations/Akash_Kumar.webp',
     },
     {
       name: "Kshitij Verma",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Kshitij_Verma.jpg',
+      image: 'src/assets/images/Operations/Kshitij_Verma.webp',
     },
     {
       name: "Nitish Kumar",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Nitish_Kumar.jpg',
+      image: 'src/assets/images/Operations/Nitish_Kumar.webp',
     },
     {
       name: "Piyush Agarwal",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Piyush_Agarwal.jpg',
+      image: 'src/assets/images/Operations/Piyush_Agarwal.webp',
     },
     {
       name: "Swarnim Gupta",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Swarnim_Gupta.jpg',
+      image: 'src/assets/images/Operations/Swarnim_Gupta.webp',
     },
     {
       name: "Viplav Kumar",
       committee: "Operations",
-      image: 'src/assets/images/Operations/Viplav_Kumar.jpg',
+      image: 'src/assets/images/Operations/Viplav_Kumar.webp',
     },
     // Sponsorships committee
     {
       name: "Aadya Nagar",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Aadya_Nagar.jpg',
+      image: 'src/assets/images/Sponsorships/Aadya_Nagar.webp',
     },
     {
       name: "Arnav Kedia",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Arnav_Kedia.jpg',
+      image: 'src/assets/images/Sponsorships/Arnav_Kedia.webp',
     },
     {
       name: "Churchit Goyal",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Churchit_Goyal.jpg',
+      image: 'src/assets/images/Sponsorships/Churchit_Goyal.webp',
     },
     {
       name: "Mahi Sachdeva",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Mahi_Sachdeva.jpg',
+      image: 'src/assets/images/Sponsorships/Mahi_Sachdeva.webp',
     },
     {
       name: "Preetansh Singh",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Preetansh_Singh.jpg',
+      image: 'src/assets/images/Sponsorships/Preetansh_Singh.webp',
     },
     {
       name: "Siddham Gupta",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Siddham_Gupta.jpg',
+      image: 'src/assets/images/Sponsorships/Siddham_Gupta.webp',
     },
     {
       name: "Tejal Salathia",
       committee: "Sponsorships",
-      image: 'src/assets/images/Sponsorships/Tejal_Salathia.jpg',
+      image: 'src/assets/images/Sponsorships/Tejal_Salathia.webp',
+    },
+    // Media committee
+    {
+      name: "Arif Islam Shaik",
+      committee: "Media",
+      image: 'src/assets/images/Media/Arif_Islam_Shaik.webp',
     },
     {
-      name: "Mahadevan KS",
-      committee: "Web Dev",
-      image: 'src/assets/images/WebDev/Mahadevan_KS.jpg',
+      name: "Rishit Jaiswal",
+      committee: "Media",
+      image: 'src/assets/images/Media/Rishit_Jaiswal.webp',
     }
 ];
 
--- vite-project/src/components/ExecCard.jsx
+++ vite-project/src/components/ExecCard.jsx
@@ -1,54 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ScrollTrigger from 'react-scroll-trigger';
 import teamMembers from './data';
 
 const ExecCard = () => {
   // Group team members by committee
-  const groupedMembers = teamMembers.reduce((acc, member) => { //accumulator
+  const groupedMembers = teamMembers.reduce((acc, member) => {
     if (!acc[member.committee]) {
-      acc[member.committee] = []; // initialize an array for this committee if it doesn't exist
+      acc[member.committee] = [];
     }
-    acc[member.committee].push(member); // add the current member to the committee array
+    acc[member.committee].push(member);
     return acc;
   }, {});
+
+  const [visibleCommittees, setVisibleCommittees] = useState({});
+
+  const onEnterViewport = (committee) => {
+    setVisibleCommittees(prev => ({ ...prev, [committee]: true }));
+  }; //scroll based cards rise upwards -> uses react-scroll-trigger
 
   return (
     <div className="flex flex-col items-center gap-16 mt-16 py-12">
       {Object.entries(groupedMembers).map(([committee, members]) => (
-        <div key={committee} className="w-full">
-          {/* Committee Banner */}
-          <div className="bg-gradient-to-r from-blue-200 to-white w-64 md:w-[350px] max-w-md mx-auto text-center rounded-md p-6 shadow-lg z-10 mb-12">
-            <p className="text-3xl font-bold text-gray-900">{committee}</p>
-          </div>
+        <ScrollTrigger
+          key={committee}
+          onEnter={() => onEnterViewport(committee)}
+          triggerOnce
+        >
+          <div className={`w-full transition-opacity duration-1000 ease-out ${visibleCommittees[committee] ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-16'}`}>
+            {/* Committee Banner */}
+            <div className="bg-gradient-to-r from-blue-200 to-white w-64 md:w-[350px] max-w-md mx-auto text-center rounded-md p-6 shadow-lg z-10 mb-12">
+              <p className="text-3xl font-bold text-gray-900">{committee}</p>
+            </div>
 
-          {/* Member Cards */}
-          <div className="flex flex-wrap justify-center gap-12">
-            {members.map((member, index) => (
-              <div key={index} className="flex flex-col items-center relative">
-                {/* Outer Card Container */}
-                <div className="w-[300px] h-[400px] rounded-lg bg-gradient-to-b from-gray-800 to-black p-4 shadow-xl">
-                  {/* Inner Card with Image and Red Border */}
-                  <div className="w-full h-full rounded-lg relative">
-                    {/* Red border effect */}
-                    <div className="absolute inset-0 border-2 border-red-500 rounded-lg"></div>
-                    {/* Image */}
-                    <img
-                      src={member.image}
-                      alt={member.name}
-                      className="w-full h-full object-cover rounded-lg"
-                    />
+            {/* Member Cards */}
+            <div className="flex flex-wrap justify-center gap-12">
+              {members.map((member, index) => (
+                <div 
+                  key={index} 
+                  className={`flex flex-col items-center relative transition-all duration-1000 ease-out delay-${index * 100}`}
+                  style={{
+                    transitionDelay: `${index * 100}ms`,
+                    opacity: visibleCommittees[committee] ? 1 : 0,
+                    transform: visibleCommittees[committee] ? 'translateY(0)' : 'translateY(50px)'
+                  }}
+                >
+                  {/* Outer Card Container */}
+                  <div className="w-[300px] h-[400px] rounded-lg bg-gradient-to-b from-gray-800 to-black p-4 shadow-xl">
+                    {/* Inner Card with Image and Red Border */}
+                    <div className="w-full h-full rounded-lg relative">
+                      {/* Red border effect */}
+                      <div className="absolute inset-0 border-2 border-red-500 rounded-lg"></div>
+                      {/* Image */}
+                      <img
+                        src={member.image}
+                        alt={member.name}
+                        className="w-full h-full object-cover rounded-lg"
+                      />
+                    </div>
+                  </div>
+
+                  {/* Name Banner */}
+                  <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-white w-48 text-center rounded-md p-2 shadow-lg z-10">
+                    <p className="text-lg font-bold text-gray-900">{member.name}</p>
+                    <p className="text-sm text-red-600">{member.role}</p>
                   </div>
                 </div>
-
-                {/* Name Banner */}
-                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-white w-48 text-center rounded-md p-2 shadow-lg z-10">
-                  <p className="text-lg font-bold text-gray-900">{member.name}</p>
-                  <p className="text-sm text-red-600">{member.role}</p>
-                </div>
-
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        </ScrollTrigger>
       ))}
     </div>
   );
